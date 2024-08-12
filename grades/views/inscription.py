1897ad748d266c0b19582c97de8483da7ecd05df
Demonqilin <velezloaizaesteban9012015@gmail.com>
2024-08-12 16:00:10 -0500

    feat: Check prerequisites before creating an inscription

--- grades/views/inscription.py
+++ grades/views/inscription.py
@@ -4,7 +4,19 @@ from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 
 from grades.serializers import inscription
-from grades.models import Course, Student, Inscription, CourseInscription
+from grades.models import Course, Student, Inscription, CourseInscription, Subject, Restriction, CourseInscription
+
+
+def student_meets_prerequisites(student: Student, subject: Subject) -> bool:
+    prerequisite_subjects = Restriction.objects.filter(
+        subject=subject).values_list('restriction', flat=True)
+    if not prerequisite_subjects.exists():
+        return True
+
+    approved_subjects = CourseInscription.objects.filter(
+        inscription__student=student, status=CourseInscription.Status.APPROVED).values_list('course__subject', flat=True)
+
+    return all(prerequisite_subject in approved_subjects for prerequisite_subject in prerequisite_subjects)
 
 
 @swagger_auto_schema(
@@ -29,6 +41,11 @@ def create_inscription(request):
     courses = Course.objects.filter(id__in=course_ids)
     if len(courses) != len(course_ids):
         return Response({'error': 'One or more courses do not exist.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    prerequisite_errors = [
+        f'Not meet prerequisites for course {course.subject.name}' for course in courses if not student_meets_prerequisites(student, course.subject)]
+    if prerequisite_errors:
+        return Response({'error': prerequisite_errors}, status=status.HTTP_400_BAD_REQUEST)
 
     new_inscription = Inscription.objects.create(student=student)
 
